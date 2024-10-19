33d5f9f90119983f9a0387e05a331a2fbf97cc18
Alina <ms.sciurus@gmail.com>
2024-10-19 19:15:51 +0300

    add swiper component

--- src/components/HeroSwiper/HeroSwiper.jsx
+++ src/components/HeroSwiper/HeroSwiper.jsx
@@ -2,49 +2,58 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './HeroSwiper.module.scss'
 import 'swiper/css';
 import 'swiper/css/mousewheel';
+import 'swiper/css/navigation';
 import { Navigation, Mousewheel } from 'swiper/modules';
+
+const arrContent = [
+	{
+		image: 'hero-img.png',
+		title: 'enjoy life sip by sip',
+		alt: 'Coffe and juice'
+	},
+	{
+		image: 'slider-2.jpg',
+		title: 'your best of choice',
+		alt: 'Coffe and man'
+	},
+	{
+		image: 'slider-3.jpg',
+		title: 'For your work flow',
+		alt: 'Coffe and work'
+	},
+];
 
 
 function HeroSwiper() {
 	return (
 		<Swiper 
-			navigation={true}
+			navigation={{
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+      }}
 			modules={[Navigation, Mousewheel]}
 			className="mySwiper"
 			scrollbar={{ draggable: true }}
 			// mousewheel={true}
 			slidesPerView={1}
 		>
-			<SwiperSlide>
-				<div className={styles.wrapper}> 
-					<div>
-						<img className={styles.image} src="/src/assets/hero-img.png" alt="Coffe and juice" />
-					</div>
-					<div className={styles.info}>
-						<h1 className={styles.title}>enjoy <br /> life sip <br /> by sip</h1>
-					</div>
-				</div>
-			</SwiperSlide>
-			<SwiperSlide>
-				<div className={styles.wrapper}> 
-					<div className={styles.image}>
-					<img className={styles.image} src="/src/assets/slider-2.jpg" alt="Coffe and juice" />
-					</div>
-					<div className={styles.info}>
-						<h1 className={styles.title}>your <br /> best of <br /> choice</h1>
-					</div>
-				</div>
-			</SwiperSlide>
-			<SwiperSlide>
-			<div className={styles.wrapper}> 
-					<div className={styles.image}>
-					<img className={styles.image} src="/src/assets/slider-3.jpg" alt="Coffe and juice" />
-					</div>
-					<div className={styles.info}>
-						<h1 className={styles.title}>For your <br /> work <br /> flow</h1>
-					</div>
-				</div>
-			</SwiperSlide>
+			{
+				arrContent.map((item, i) => {
+					return (
+						<SwiperSlide key={i}>
+							<div className={styles.wrapper}> 
+								<div>
+									<img className={styles.image} src={`/src/assets/${item.image}`} alt={item.alt} />
+								</div>
+								<div className={styles.info}>
+									<h1 className={styles.title}>{item.title}</h1>
+								</div>
+							</div>
+						</SwiperSlide>
+					)
+				})
+			}
+			<div className='swiper-button-prev'></div>
 		</Swiper>
 	)
 }
