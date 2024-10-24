12bd8f18d88d4db6a76c6124df4e5a7858a6940a
Alina <ms.sciurus@gmail.com>
2024-10-24 19:25:22 +0300

    add coffeshops changes

--- src/components/CoffeShop/CoffeShop.jsx
+++ src/components/CoffeShop/CoffeShop.jsx
@@ -1,9 +1,29 @@
 import PropTypes from 'prop-types';
+import styles from './CoffeShop.module.scss';
+
 
 function CoffeShop({content}) {
 	return (
-		<div>
-			<h1>{content.title}</h1>
+		<div >
+			<div className={styles.wrapper}>
+				<div>
+					<img className={styles.image} src={content.image} alt="" />
+				</div>
+				<div className={styles.information}>
+					<div className={styles.infowrapper}>
+						<h1>{content.title}</h1>
+						<a>{content.adress}</a>
+						<a>{content.tel}</a>
+						<a>{content.email}</a>
+					</div>
+					<div>
+						<p>Mon-Fri..........{content.weekdays}</p>
+						<p>Sat-Sun.........{content.weekends}</p>
+					</div>
+					<button className={styles.button}>Contact us</button>
+			</div>
+			</div>
+
 		</div>
 	)
 }
--- src/components/CoffeShops/CoffeShops.jsx
+++ src/components/CoffeShops/CoffeShops.jsx
@@ -9,7 +9,7 @@ const coffeShopContent = [
 		email: 'fgfgh@gmail.com',
 		weekdays: '6.45am-3.00pm',
 		weekends: '8.30am-4.00pm',
-		image: ''
+		image: '/src/assets/coffe-image-1.jpg'
 	},
 	{
 		title: 'Berlin Coffe Roastery',
@@ -18,7 +18,7 @@ const coffeShopContent = [
 		email: 'wewe@gmail.com',
 		weekdays: '6.45am-3.00pm',
 		weekends: '8.30am-4.00pm',
-		image: ''
+		image: '/src/assets/coffe-image-2.jpg'
 	},
 	{
 		title: 'Amsterdam Coffe Shop',
@@ -27,7 +27,7 @@ const coffeShopContent = [
 		email: 'weferf@gmail.com',
 		weekdays: '6.45am-3.00pm',
 		weekends: '8.30am-4.00pm',
-		image: ''
+		image: '/src/assets/coffe-image-3.jpg'
 	},
 	{
 		title: 'Amsterdam Centre Coffe',
@@ -36,7 +36,7 @@ const coffeShopContent = [
 		email: 'eth6y@gmail.com',
 		weekdays: '6.45am-3.00pm',
 		weekends: '8.30am-4.00pm',
-		image: ''
+		image: '/src/assets/coffe-image-4.png'
 	}
 ];
 
