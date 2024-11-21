f0913bc0c7da28c452c8a4d6095a6693ee758f1a
Alina <ms.sciurus@gmail.com>
2024-11-21 15:12:43 +0200

    add follow us section

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,35 @@
 // import viteLogo from '/vite.svg'
 import "./App.scss";
 import PreHeader from "./components/PreHeader/PreHeader";
-// import Header from "./components/Header/Header";
-// import Coffe from "./components/CoffeCarts/CoffeCarts";
-// import HeroSwiper from "./components/HeroSwiper/HeroSwiper";
-// import AfterHeader from "./components/AfterHeader/AfterHeader";
-// import CoffeShops from "./components/CoffeShops/CoffeShops";
-// import Banner from "./components/Banner/Banner";
-// import MenuFood from "./components/MenuFood/MenuFood";
-// import Team from "./components/Team/Team";
-// import BlogPost from "./components/BlogPost/BlogPost";
-// import BaristaSchool from "./components/BaristaSchool/BaristaSchool";
+import Header from "./components/Header/Header";
+import Coffe from "./components/CoffeCarts/CoffeCarts";
+import HeroSwiper from "./components/HeroSwiper/HeroSwiper";
+import AfterHeader from "./components/AfterHeader/AfterHeader";
+import CoffeShops from "./components/CoffeShops/CoffeShops";
+import Banner from "./components/Banner/Banner";
+import MenuFood from "./components/MenuFood/MenuFood";
+import Team from "./components/Team/Team";
+import BlogPost from "./components/BlogPost/BlogPost";
+import BaristaSchool from "./components/BaristaSchool/BaristaSchool";
 import TrustedSupplies from "./components/TrustedSupplies/TrustedSupplies";
+import FollowUs from "./components/FollowUs/FollowUs";
 
 function App() {
 	return (
 		<>
 			<PreHeader />
-			{/* <Header />
+			<Header />
 			<HeroSwiper />
 			<AfterHeader />
 			<Banner />
 			<CoffeShops />
 			<Coffe />
 			<MenuFood />
-			<Team /> 
+			<Team />
 			<BlogPost />
-			<BaristaSchool />*/}
+			<BaristaSchool />
 			<TrustedSupplies />
+			<FollowUs />
 		</>
 	);
 }
---
+++ src/components/FollowUs/FollowUs.jsx
@@ -0,0 +1,35 @@
+import styles from "./FollowUs.module.scss";
+
+const suppliesItems = [
+	"/src/assets/follow-us/1.png",
+	"/src/assets/follow-us/2.png",
+	"/src/assets/follow-us/3.png",
+	"/src/assets/follow-us/4.png",
+	"/src/assets/follow-us/5.png",
+	"/src/assets/follow-us/6.png",
+	"/src/assets/follow-us/7.png",
+	"/src/assets/follow-us/8.png",
+	"/src/assets/follow-us/9.png",
+	"/src/assets/follow-us/10.png",
+];
+
+function FollowUs() {
+	return (
+		<div>
+			<h1 className={styles.title}>our trusted supplies</h1>
+			<ul className={styles.wrapper}>
+				{suppliesItems.map((item, i) => {
+					return (
+						<li className={styles.item} key={i}>
+							<a href="#">
+								<img src={item} alt="" />
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+
+export default FollowUs;
